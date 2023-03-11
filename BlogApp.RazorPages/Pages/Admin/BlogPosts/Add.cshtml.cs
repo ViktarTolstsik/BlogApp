b75557c943b70b787edaa6d7bd471084069c4fce
Victor Tolstik <viktor.tolstik.96@gmail.com>
2023-03-11 14:17:32 +0300

    Blog post saving to the DB implemented

--- BlogApp.RazorPages/Pages/Admin/BlogPosts/Add.cshtml.cs
+++ BlogApp.RazorPages/Pages/Admin/BlogPosts/Add.cshtml.cs
@@ -1,3 +1,5 @@
+using BlogApp.RazorPages.Data;
+using BlogApp.RazorPages.Models.Domain;
 using BlogApp.RazorPages.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,16 +8,35 @@ namespace BlogApp.RazorPages.Pages.Admin.BlogPosts
 {
     public class AddModel : PageModel
     {
+        private readonly BlogAppDbContext blogAppDbContext;
+
         [BindProperty]
         public AddBlogPost AddBlogPostRequest { get; set; }
 
+        public AddModel(BlogAppDbContext BlogAppDbContext)
+        {
+            blogAppDbContext = BlogAppDbContext;
+        }
         public void OnGet()
         {
         }
 
         public void OnPost() 
         {
-            var heading = Request.Form["heading"];
+            var blogPost = new BlogPost()
+            {
+                Heading = AddBlogPostRequest.Heading,
+                PageTitle = AddBlogPostRequest.PageTitle,
+                Content = AddBlogPostRequest.Content,
+                ShortDescription = AddBlogPostRequest.ShortDescription,
+                FeaturedImageUrl = AddBlogPostRequest.FeaturedImageUrl,
+                UrlHandle = AddBlogPostRequest.UrlHandle,
+                PublishedDate = AddBlogPostRequest.PublishedDate,
+                Author = AddBlogPostRequest.Author,
+                Visible = AddBlogPostRequest.Visible
+            };
+            blogAppDbContext.BlogPosts.Add(blogPost);
+            blogAppDbContext.SaveChanges();
         }
     }
 }
