5244d858d12770cf00dfe9f9a119a1894c48b8c5
Victor Tolstik <viktor.tolstik.96@gmail.com>
2023-03-12 10:33:41 +0300

    Cloudinary registration and settings setting completed

--- BlogApp.RazorPages/Repositories/ImageRepository.cs
+++ BlogApp.RazorPages/Repositories/ImageRepository.cs
@@ -1,6 +1,33 @@
-﻿namespace BlogApp.RazorPages.Repositories
+﻿using CloudinaryDotNet;
+
+namespace BlogApp.RazorPages.Repositories
 {
-    public class ImageRepository
+    //Cloudinary
+
+    public class ImageRepository : IImageRepository
     {
+        private readonly Account account;
+        public ImageRepository(IConfiguration configuration)
+        {
+            account = new Account(configuration.GetSection("Cloudinary")["CloudName"], 
+                configuration.GetSection("Cloudinary")["ApiKey"], 
+                configuration.GetSection("Cloudinary")["ApiSecret"]);
+        }
+        public async Task<string> UploadAsync(IFormFile file)
+        {
+            var client = new Cloudinary(account);
+            var uploadFileResult = await client.UploadAsync(new CloudinaryDotNet.Actions.ImageUploadParams()
+            {
+                File = new FileDescription(file.FileName, file.OpenReadStream()),
+                DisplayName = file.FileName
+            });
+
+            if (uploadFileResult != null && uploadFileResult.StatusCode ==System.Net.HttpStatusCode.OK) 
+            {
+                return uploadFileResult.SecureUri.ToString();
+            }
+
+            return null;
+        }
     }
 }
