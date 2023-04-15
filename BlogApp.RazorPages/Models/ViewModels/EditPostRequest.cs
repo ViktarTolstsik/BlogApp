2a00273c0e365487c67992640eaa9a3122cb73cc
Victor Tolstik <viktor.tolstik.96@gmail.com>
2023-04-15 15:01:53 +0300

    Edit Post page validations implemented

---
+++ BlogApp.RazorPages/Models/ViewModels/EditPostRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.RazorPages.Models.ViewModels
+{
+	public class EditPostRequest
+	{
+		[Required]
+		public Guid Id { get; set; }
+		[Required]
+		public string Heading { get; set; }
+		[Required]
+		public string PageTitle { get; set; }
+		[Required]
+		public string Content { get; set; }
+		[Required]
+		public string ShortDescription { get; set; }
+		[Required]
+		public string FeaturedImageUrl { get; set; }
+		[Required]
+		public string UrlHandle { get; set; }
+		[Required]
+		public DateTime PublishedDate { get; set; }
+		[Required]
+		public string Author { get; set; }
+		public bool Visible { get; set; }
+	}
+}
--- BlogApp.RazorPages/Pages/Admin/BlogPosts/Edit.cshtml.cs
+++ BlogApp.RazorPages/Pages/Admin/BlogPosts/Edit.cshtml.cs
@@ -5,67 +5,102 @@ using BlogApp.RazorPages.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace BlogApp.RazorPages.Pages.Admin.BlogPosts
 {
-    [Authorize(Roles = "Admin")]
-    public class EditModel : PageModel
-    {
+	[Authorize(Roles = "Admin")]
+	public class EditModel : PageModel
+	{
 		private readonly IBlogPostRepository blogPostRepository;
 
 		[BindProperty]
-        public BlogPost BlogPost { get; set; }
+		public EditPostRequest BlogPost { get; set; }
 
-        [BindProperty]
-        public IFormFile FeaturedImage { get; set; }
+		[BindProperty]
+		public IFormFile FeaturedImage { get; set; }
 
-        [BindProperty]
-        public string Tags { get; set; }
-        public EditModel(IBlogPostRepository BlogPostRepository)
-        {
+		[BindProperty]
+		[Required]
+		public string Tags { get; set; }
+		public EditModel(IBlogPostRepository BlogPostRepository)
+		{
 			blogPostRepository = BlogPostRepository;
 		}
 
-        public async Task OnGet(Guid Id)
-        {
-            BlogPost = await blogPostRepository.GetPostAsync(Id);
+		public async Task OnGet(Guid Id)
+		{
+			var blogPostDomainModel = await blogPostRepository.GetPostAsync(Id);
 
-            if (BlogPost.Tags != null  && BlogPost != null) 
-            {
-                Tags = string.Join(",", BlogPost.Tags.Select(x => x.Name));
-            }
-        }
-        public async Task<IActionResult> OnPostEdit()
-        {
-            try
-            {
-                BlogPost.Tags = new List<Tag> (Tags.Split(',').Select(x => new Tag() { Name = x.Trim()}));
-
-                await blogPostRepository.UpdatePostAsync(BlogPost);
-
-                ViewData["MessageDescription"] = new Notification
-                {
-                    Message = "Update was successful!",
-                    Type = Enums.NotificationType.Success
-                };
-            }
-            catch (Exception ex)
-            {
-				ViewData["MessageDescription"] = new Notification
+			if (blogPostDomainModel != null && blogPostDomainModel.Tags != null)
+			{
+				BlogPost = new EditPostRequest
 				{
-					Message = ex.Message,
-					Type = Enums.NotificationType.Error
+					Id = blogPostDomainModel.Id,
+					Heading = blogPostDomainModel.Heading,
+					PageTitle = blogPostDomainModel.PageTitle,
+					Content = blogPostDomainModel.Content,
+					ShortDescription = blogPostDomainModel.ShortDescription,
+					FeaturedImageUrl = blogPostDomainModel.FeaturedImageUrl,
+					UrlHandle = blogPostDomainModel.UrlHandle,
+					PublishedDate = blogPostDomainModel.PublishedDate,
+					Author = blogPostDomainModel.Author,
+					Visible = blogPostDomainModel.Visible
 				};
-            }
 
-            return Page();
-        }
-        public async Task<IActionResult> OnPostDelete() 
-        {
-            var deleted = await blogPostRepository.DeletePostAsync(BlogPost.Id);
-            if (deleted)
-            {
+				Tags = string.Join(",", blogPostDomainModel.Tags.Select(x => x.Name));
+			}
+		}
+		public async Task<IActionResult> OnPostEdit()
+		{
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					var blogPostDomainModel = new BlogPost
+					{
+						Id = BlogPost.Id,
+						PageTitle = BlogPost.PageTitle,
+						Heading = BlogPost.Heading,
+						Content = BlogPost.Content,
+						ShortDescription = BlogPost.ShortDescription,
+						FeaturedImageUrl = BlogPost.FeaturedImageUrl,
+						UrlHandle = BlogPost.UrlHandle,
+						PublishedDate = BlogPost.PublishedDate,
+						Author = BlogPost.Author,
+						Visible = BlogPost.Visible,
+						Tags = new List<Tag>(Tags.Split(',').Select(x => new Tag() { Name = x.Trim() }))
+					};
+
+
+					await blogPostRepository.UpdatePostAsync(blogPostDomainModel);
+
+					ViewData["MessageDescription"] = new Notification
+					{
+						Message = "Update was successful!",
+						Type = Enums.NotificationType.Success
+					};
+				}
+				catch (Exception ex)
+				{
+					ViewData["MessageDescription"] = new Notification
+					{
+						Message = ex.Message,
+						Type = Enums.NotificationType.Error
+					};
+				}
+
+				return Page();
+			}
+
+			return Page();
+		}
+		public async Task<IActionResult> OnPostDelete()
+		{
+			var deleted = await blogPostRepository.DeletePostAsync(BlogPost.Id);
+			if (deleted)
+			{
 				var notification = new Notification
 				{
 					Type = Enums.NotificationType.Success,
@@ -76,9 +111,9 @@ namespace BlogApp.RazorPages.Pages.Admin.BlogPosts
 				TempData["MessageDescription"] = JsonSerializer.Serialize(notification);
 
 				return RedirectToPage("/admin/blogposts/list");
-            }
+			}
 
-            return Page();
-        }
-    }
+			return Page();
+		}
+	}
 }
